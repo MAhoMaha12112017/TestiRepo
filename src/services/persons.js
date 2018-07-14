e59ca3654d83c409617f0b656e477f607f95c254
Matti Aho <ookooko@gmail.com>
2018-07-14 06:00:00 +0300

    for Heroku

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // const baseUrl = 'http://localhost:3001/persons';  // with JSON Server
-const baseUrl = 'http://localhost:3001/api/persons'; // with cors and node
+// const baseUrl = 'http://localhost:3001/api/persons'; // with cors and node
+const baseUrl = '/api/persons'; // with Heroku
 
 const getAll = () => {
   return axios.get(baseUrl).then((r) => r.data);
